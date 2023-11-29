df48decdc846c87f491a98b32dcebf795e1e08af
Nassm3 <98339153+Nassm3@users.noreply.github.com>
2023-11-29 16:56:58 +0100

    Update shiny.js

--- shiny.js
+++ shiny.js
@@ -124,9 +124,10 @@ import('https://webr.r-wasm.org/v0.2.2/webr.mjs').then(async ({ WebR }) => {
   await fetchToWebR('app/node.csv', '/home/web_user/app/node.csv');
 
   // Install and run shiny
-  await webR.evalRVoid(`webr::mount("/shiny", "${window.location.href}/image/library.data")`);
+  //await webR.evalRVoid(`webr::mount("/shiny", "${window.location.href}/image/library.data")`);
+  //.libPaths(c("/shiny", .libPaths()))
+  await webR.installPackages(['shiny', 'visNetwork'])
   webR.writeConsole(`
-    .libPaths(c("/shiny", .libPaths()))
     library(shiny)
     library(visNetwork)
     options(shiny.trace = TRUE)
